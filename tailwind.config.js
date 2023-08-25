7b08aad864fe2986184a13362f10518527afc7e7
Christoffer <cgranzell@hotmail.com>
2023-08-25 13:57:47 +0200

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        "deep-blue": "000534",
+
+      },
+      fontFamily: {
+        
+      }
+    },
+  },
+  plugins: [],
+}
